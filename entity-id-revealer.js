682d9fc39d65fff48f4b839efe1552b7c59c5faf
PRyC <prycpl@gmail.com>
2026-07-18 18:17:12 +0200

    Update entity-id-revealer.js

--- entity-id-revealer.js
+++ entity-id-revealer.js
@@ -28,109 +28,129 @@
     root.appendChild(s);
   }
 
-  function observeWarning(el) {
-    if (el._erWatched) return;
-    el._erWatched = true;
-    var obs = new MutationObserver(function () {
-      patchWarning(el);
-    });
-    obs.observe(el, { childList: true, characterData: true, subtree: true });
-    el._erObserver = obs;
-  }
-
   function patchWarning(el) {
     var host = el.getRootNode().host;
-    if (!host) {
-      console.log("[er] patchWarning: no host", el);
-      return;
-    }
+    if (!host) return;
     var config = host._config || host.config;
-    if (!config || !config.entity) {
-      console.log("[er] patchWarning: no config/entity", el, host);
-      return;
-    }
+    if (!config || !config.entity) return;
     var entityId = config.entity;
-    console.log("[er] patchWarning: entityId=" + entityId, el);
 
     if (el.tagName === "HUI-WARNING") {
-      observeWarning(el);
       var text = el.textContent || "";
-      console.log("[er] HUI-WARNING text=" + text + " isNotFound=" + isNotFoundMessage(text) + " alreadyHasId=" + (text.indexOf(entityId) !== -1));
       if (isNotFoundMessage(text) && text.indexOf(entityId) === -1) {
         el.innerHTML = text.replace(/:*\s*$/, "") + ":<br>" + entityId;
-        console.log("[er] patched HUI-WARNING to:", el.innerHTML);
       }
-      var errorCard = el.shadowRoot && el.shadowRoot.querySelector("hui-error-card");
+      var errorCard =
+        el.shadowRoot && el.shadowRoot.querySelector("hui-error-card");
       if (errorCard) fixErrorCard(errorCard);
     } else if (el.tagName === "HUI-WARNING-ELEMENT") {
       var label = el.label || "";
-      console.log("[er] HUI-WARNING-ELEMENT label=" + label);
       if (isNotFoundMessage(label) && label.indexOf(entityId) === -1) {
         el.label = label.replace(/:*\s*$/, "") + ": " + entityId;
-        console.log("[er] patched HUI-WARNING-ELEMENT to:", el.label);
       }
     }
   }
 
-  function scanForWarnings(root, label) {
-    var warnings = root.querySelectorAll("hui-warning, hui-warning-element");
-    console.log("[er] scanForWarnings(" + label + "): found " + warnings.length + " warnings, root:", root);
+  var observedRoots = [];
+
+  function scanNode(node) {
+    if (node.nodeType !== 1) return;
+    var tag = node.tagName;
+    if (tag === "HUI-WARNING" || tag === "HUI-WARNING-ELEMENT") {
+      patchWarning(node);
+    }
+    if (node.shadowRoot) {
+      observeRoot(node.shadowRoot);
+    }
+    var warnings = node.querySelectorAll("hui-warning, hui-warning-element");
     for (var i = 0; i < warnings.length; i++) {
       patchWarning(warnings[i]);
-      if (warnings[i].tagName === "HUI-WARNING") observeWarning(warnings[i]);
     }
+    var all = node.querySelectorAll("*");
+    for (var i = 0; i < all.length; i++) {
+      if (all[i].shadowRoot) {
+        observeRoot(all[i].shadowRoot);
+      }
+    }
+  }
+
+  function observeRoot(root) {
+    if (root._erObserved) return;
+    root._erObserved = true;
+    observedRoots.push(root);
+
+    var warnings = root.querySelectorAll("hui-warning, hui-warning-element");
+    for (var i = 0; i < warnings.length; i++) {
+      patchWarning(warnings[i]);
+    }
+    var all = root.querySelectorAll("*");
+    for (var i = 0; i < all.length; i++) {
+      if (all[i].shadowRoot) {
+        observeRoot(all[i].shadowRoot);
+      }
+    }
+
+    var observer = new MutationObserver(function (mutations) {
+      for (var i = 0; i < mutations.length; i++) {
+        var m = mutations[i];
+        if (m.type === "childList") {
+          for (var j = 0; j < m.addedNodes.length; j++) {
+            scanNode(m.addedNodes[j]);
+          }
+          for (var j = 0; j < m.removedNodes.length; j++) {
+            cleanupRemoved(m.removedNodes[j]);
+          }
+        } else if (m.type === "characterData") {
+          var p = m.target.parentElement;
+          if (p && (p.tagName === "HUI-WARNING" || p.tagName === "HUI-WARNING-ELEMENT")) {
+            patchWarning(p);
+          }
+        }
+      }
+    });
+    observer.observe(root, { childList: true, subtree: true, characterData: true });
+  }
+
+  function cleanupRemoved(node) {
+    if (node.nodeType !== 1) return;
+    if (node.shadowRoot && node.shadowRoot._erObserved) {
+      observedRoots = observedRoots.filter(function (r) { return r !== node.shadowRoot; });
+    }
+    var all = node.querySelectorAll("*");
+    for (var i = 0; i < all.length; i++) {
+      if (all[i].shadowRoot && all[i].shadowRoot._erObserved) {
+        observedRoots = observedRoots.filter(function (r) { return r !== all[i].shadowRoot; });
+      }
+    }
+  }
+
+  var lightObserver = new MutationObserver(function (mutations) {
+    for (var i = 0; i < mutations.length; i++) {
+      var m = mutations[i];
+      if (m.type !== "childList") continue;
+      for (var j = 0; j < m.addedNodes.length; j++) {
+        scanNode(m.addedNodes[j]);
+      }
+    }
+  });
+  lightObserver.observe(document.body, { childList: true, subtree: true });
+
+  function scanForWarnings(root) {
+    var warnings = root.querySelectorAll("hui-warning, hui-warning-element");
+    for (var i = 0; i < warnings.length; i++) patchWarning(warnings[i]);
     var children = root.querySelectorAll("*");
-    var shadowCount = 0;
     for (var i = 0; i < children.length; i++) {
       if (children[i].shadowRoot) {
-        shadowCount++;
-        scanForWarnings(children[i].shadowRoot, label + ">" + (children[i].tagName || children[i].localName));
+        observeRoot(children[i].shadowRoot);
+        scanForWarnings(children[i].shadowRoot);
       }
-    }
-    console.log("[er] scanForWarnings(" + label + "): recursed into " + shadowCount + " shadow roots");
-  }
-
-  function patchElement(name) {
-    var patch = function () {
-      var Cls = customElements.get(name);
-      if (!Cls) {
-        console.log("[er] patchElement(" + name + "): not defined yet");
-        return false;
-      }
-      var orig = Cls.prototype.connectedCallback;
-      if (orig._erPatched) {
-        console.log("[er] patchElement(" + name + "): already patched");
-        return true;
-      }
-      Cls.prototype.connectedCallback = function () {
-        orig.apply(this, arguments);
-        console.log("[er] connectedCallback(" + name + "): triggered, scheduling patchWarning");
-        var self = this;
-        var attempts = [0, 50, 200];
-        for (var a = 0; a < attempts.length; a++) {
-          setTimeout(patchWarning, attempts[a], self);
-        }
-      };
-      Cls.prototype.connectedCallback._erPatched = true;
-      console.log("[er] patchElement(" + name + "): patched successfully (sync)");
-      return true;
-    };
-    if (!patch()) {
-      customElements.whenDefined(name).then(function () {
-        console.log("[er] patchElement(" + name + "): whenDefined resolved, now patching");
-        patch();
-      });
     }
   }
 
-  console.log("[er] === STARTUP ===");
-  patchElement("hui-warning");
-  patchElement("hui-warning-element");
-
-  scanForWarnings(document.body, "body");
-  setTimeout(function () { scanForWarnings(document.body, "body-500"); }, 500);
-  setTimeout(function () { scanForWarnings(document.body, "body-1500"); }, 1500);
-  setTimeout(function () { console.log("[er] === FINAL SCAN ==="); scanForWarnings(document.body, "body-3000"); }, 3000);
+  scanForWarnings(document.body);
+  setTimeout(function () { scanForWarnings(document.body); }, 500);
+  setTimeout(function () { scanForWarnings(document.body); }, 1500);
+  setTimeout(function () { scanForWarnings(document.body); }, 3000);
 
   window.__entityIdRevealerActive = true;
 })();
